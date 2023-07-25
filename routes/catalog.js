5d8a46249af3e7740f52f8523e99847d47efe117
Papin Semyon <s.papin@sftpro.ru>
2023-07-25 12:47:22 +0300

    delete space useless

--- routes/catalog.js
+++ routes/catalog.js
@@ -36,7 +36,7 @@ router.get('/', async (req, res) => {
 		dateReleased,
 		categoryAlias,
 	} = req.query;
-  
+
 	let sortParams = { raisedUpAt: -1, createdAt: -1 }; // Параметры сортировки
 
 	const skip = +req.query.skip || 0
