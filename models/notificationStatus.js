d806ee80e9fe6b433506ecde214a88455f13bccd
Papin Semyon <s.papin@sftpro.ru>
2023-08-11 21:32:02 +0300

    добавил пояснения для поля status в коллекции NotificationStatus

--- models/notificationStatus.js
+++ models/notificationStatus.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const notoficationStatusSchema = new mongoose.Schema({
 	notificationId: mongoose.Schema.Types.ObjectId,
 	userId: mongoose.Schema.Types.ObjectId,
-	status: String,
+	status: String, //read или sent
 }, {
 	timestamps: true
 })
