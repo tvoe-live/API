1db7ea56d05248aaa6de3082cd3cbc52a7766c72
Papin Semyon <s.papin@sftpro.ru>
2023-09-06 20:38:13 +0300

    initial

--- routes/profile/history.js
+++ routes/profile/history.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Movie = require('../../models/movie');
+const MoviePageLog = require('../../models/moviePageLog');
 const verify = require('../../middlewares/verify');
 const resError = require('../../helpers/resError');
 const movieOperations = require('../../helpers/movieOperations');
@@ -9,37 +10,104 @@ const movieOperations = require('../../helpers/movieOperations');
  * Профиль > История просмотров
  */
 
+// router.get('/', verify.token, async (req, res) => {
+// 	const skip = +req.query.skip || 0
+// 	const limit = +(req.query.limit > 0 && req.query.limit <= 100 ? req.query.limit : 100)
+
+// 	const agregationListForTotalSize = [
+// 		{ $lookup: {
+// 			from: "moviepagelogs",
+// 			localField: "_id",
+// 			foreignField: "movieId",
+// 			pipeline: [
+// 				{ $match: { userId: req.user._id } },
+// 				{ $group: {
+// 					_id: "$movieId",
+// 					count: { $sum: 1 },
+// 					updatedAt: { $push: "$$ROOT.updatedAt" }
+// 				} },
+// 				{ $sort: { updatedAt: -1 } }
+// 			],
+// 			as: "moviepagelogs"
+// 		} },
+// 		{ $unwind: "$moviepagelogs" },
+// 	]
+
+// 	try {
+// 		Movie.aggregate([
+// 			{
+// 				"$facet": {
+// 					"totalSize":[
+// 						...agregationListForTotalSize,
+// 						{ $group: {
+// 							_id: null,
+// 							count: { $sum: 1 }
+// 						} },
+// 						{ $project: { _id: false } },
+// 						{ $limit: 1 }
+// 					],
+// 					"items":[
+// 						...agregationListForTotalSize,
+// 						...movieOperations({
+// 							addToProject: {
+// 								poster: { src: true },
+// 								moviepagelogs: "$moviepagelogs",
+// 								lastLogUpdatedAt: "$moviepagelogs.updatedAt"
+// 							},
+// 							skip,
+// 							limit
+// 						}),
+// 						{ $sort: { lastLogUpdatedAt: -1 } },
+// 					]
+// 				}
+// 			},
+// 			{ $unwind: { path: "$totalSize", preserveNullAndEmptyArrays: true } },
+// 			{ $project: {
+// 				totalSize: { $cond: [ "$totalSize.count", "$totalSize.count", 0] },
+// 				items: "$items"
+// 			} },
+// 		], (err, result)=>{
+// 			return res.status(200).json(result[0]);
+// 		});
+
+// 	} catch(err) {
+// 		return resError({ res, msg: err });
+// 	}
+// });
+
 router.get('/', verify.token, async (req, res) => {
 	const skip = +req.query.skip || 0
 	const limit = +(req.query.limit > 0 && req.query.limit <= 100 ? req.query.limit : 100)
 
 	const agregationListForTotalSize = [
+		{
+			$match: {
+				userId: req.user._id
+			}
+		},
 		{ $lookup: {
-			from: "moviepagelogs",
-			localField: "_id",
-			foreignField: "movieId",
+			from: "movies",
+			localField: "movieId",
+			foreignField: "_id",
 			pipeline: [
-				{ $match: { userId: req.user._id } },
-				{ $group: { 
-					_id: "$movieId", 
-					count: { $sum: 1 }, 
-					updatedAt: { $push: "$$ROOT.updatedAt" }
-				} },
-				{ $sort: { updatedAt: -1 } }
+				{ $project: { persons:false } },
 			],
-			as: "moviepagelogs"
+			as: "movie"
 		} },
-		{ $unwind: "$moviepagelogs" },
+		{ $unwind: "$movie" },
+		{$group:{
+			_id:"$movie._id"
+		}}
 	]
 
 	try {
-		Movie.aggregate([
+		MoviePageLog.aggregate([
 			{
 				"$facet": {
 					"totalSize":[
 						...agregationListForTotalSize,
-						{ $group: { 
-							_id: null, 
+						{ $group: {
+							_id: null,
 							count: { $sum: 1 }
 						} },
 						{ $project: { _id: false } },
@@ -47,16 +115,16 @@ router.get('/', verify.token, async (req, res) => {
 					],
 					"items":[
 						...agregationListForTotalSize,
-						...movieOperations({
-							addToProject: {
-								poster: { src: true },
-								moviepagelogs: "$moviepagelogs",
-								lastLogUpdatedAt: "$moviepagelogs.updatedAt"
-							},
-							skip,
-							limit
-						}),
-						{ $sort: { lastLogUpdatedAt: -1 } },
+						// ...movieOperations({
+						// 	addToProject: {
+						// 		poster: { src: true },
+						// 		moviepagelogs: "$moviepagelogs",
+						// 		lastLogUpdatedAt: "$moviepagelogs.updatedAt"
+						// 	},
+						// 	skip,
+						// 	limit
+						// }),
+						// { $sort: { lastLogUpdatedAt: -1 } },
 					]
 				}
 			},
@@ -68,7 +136,7 @@ router.get('/', verify.token, async (req, res) => {
 		], (err, result)=>{
 			return res.status(200).json(result[0]);
 		});
-		
+
 	} catch(err) {
 		return resError({ res, msg: err });
 	}
