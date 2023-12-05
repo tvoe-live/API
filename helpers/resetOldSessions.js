3920c2ddafa25f79537acc713eebc10709c593c8
atlszxc <kiakovenko.reon@gmail.com>
2023-12-05 16:03:40 +0300

    Исправленны опчатки и запрос на получение пользователей

--- helpers/resetOldSessions.js
+++ helpers/resetOldSessions.js
@@ -3,11 +3,19 @@ const user = require('../models/user')
 const resetOldSessions = async () => {
 	try {
 		const users = await user.find({
-			sessions: { $elemMatch: { lastVisitAt: { $lte: new Date() - 1000 * 60 * 60 * 24 * 90 } } },
+			sessions: {
+				$elemMatch: {
+					lastVisitAt: {
+						$lte: new Date() - 1000 * 60 * 60 * 24 * Number(process.env.SESSIONS_DAYS),
+					},
+				},
+			},
 		})
 		for (const usr of users) {
 			usr.sessions = usr.sessions.filter(
-				(session) => session.lastVisitAt >= new Date() - 1000 * 60 * 60 * 24 * 90
+				(session) =>
+					session.lastVisitAt >=
+					new Date() - 1000 * 60 * 60 * 24 * Number(process.env.SESSIONS_DAYS)
 			)
 			await usr.save()
 		}
