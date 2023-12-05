f104db9b2b7df558b506a84f1234aecbec707a62
atlszxc <kiakovenko.reon@gmail.com>
2023-12-05 10:45:41 +0300

    Добавлены задачи на чистку старых сессий и истекших подписок

---
+++ helpers/resetOldSessions.js
@@ -0,0 +1,19 @@
+const user = require('../models/user')
+
+const resetOldSessions = async () => {
+	try {
+		const users = await user.find({
+			sessions: { $elemMatch: { lastVisitAt: { $lte: new Date() - 1000 * 60 * 60 * 24 * 90 } } },
+		})
+		for (const usr of users) {
+			usr.sessions = usr.sessions.filter(
+				(session) => session.lastVisitAt >= new Date() - 1000 * 60 * 60 * 24 * 90
+			)
+			await usr.save()
+		}
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+module.exports = resetOldSessions
---
+++ helpers/resetSubscribeTask.js
@@ -0,0 +1,24 @@
+const user = require('../models/user')
+
+const resetSubscribe = async () => {
+	const start = new Date()
+	const finish = new Date(start - 3 * 60 * 1000)
+
+	try {
+		const users = await user.find(
+			{
+				'subscribe.finishAt': { $lt: start, $gte: finish },
+			},
+			{ subscribe: true }
+		)
+
+		for (const usr of users) {
+			usr.subscribe = null
+			await usr.save()
+		}
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+module.exports = resetSubscribe
